eb8048b3e00338ad63213e9b7a56e567d8621870
Cornelius Soon <cornelius.soon@gmail.com>
2016-09-08 08:54:57 +1200

    Complete web guesser with 2 features

--- web_guesser.rb
+++ web_guesser.rb
@@ -4,31 +4,65 @@ require 'sinatra/reloader'
 get '/' do
   player_guess = params["guess"].to_i
   response = check_guess(player_guess)
+  @@guesses_remaining -= 1
+  evaluate_guesses(player_guess)
   
-  erb :index, :locals => {:answer               => RANDOM_NUMBER, 
-                          :player_guess         => params["guess"],
-                          :response             => response}
+  
+      erb :index, :locals => {:answer               => @@random_number,
+                              :player_guess         => params["guess"],
+                              :response             => response,
+                              :color_response       => @color_response,
+                              :guesses_remaining    => @@guesses_remaining,
+                              :message              => @message}
 end
 
-RANDOM_NUMBER = rand(100) + 1
+
+
+
+@@random_number = rand(100) + 1
+@@guesses_remaining = 6
+
+
+def evaluate_guesses(player_guess)
+  if player_guess == @@random_number
+    reset_game
+    @message = "You won!! -- A new number has been generated and your guesses remaining reset."
+  end
+  
+  if @@guesses_remaining == 0
+    reset_game
+    @message = "You lost!! -- A new number has been generated and your guesses remaining reset."
+  end
+end
+
+def reset_game
+  @@guesses_remaining = 5
+  @@random_number = rand(100) + 1
+end
+
 
 
 def check_guess(player_guess)
   if player_guess == "".to_i
     ""
-  elsif player_guess == RANDOM_NUMBER
-    "Response: The secret number was #{RANDOM_NUMBER}"
-  elsif player_guess < RANDOM_NUMBER
-    if player_guess < RANDOM_NUMBER - 5
-      "Response: Much too low"
+  elsif player_guess == @@random_number
+    @color_response = "response__paragraph--correct"
+    "The secret number was #{@@random_number}"
+  elsif player_guess < @@random_number
+    if player_guess < @@random_number - 5
+      @color_response = "response__paragraph--far"
+      "Much too low"
     else
-      "Response: Too low"
+      @color_response = "response__paragraph--near"
+      "Too low"
     end
-  elsif player_guess > RANDOM_NUMBER
-    if player_guess > RANDOM_NUMBER + 5
-      "Response: Much too high"
+  elsif player_guess > @@random_number
+    if player_guess > @@random_number + 5
+      @color_response = "response__paragraph--far"
+      "Much too high"
     else
-      "Response: Too high"
+      @color_response = "response__paragraph--near"
+      "Too high"
     end
   end
 end
